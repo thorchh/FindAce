2f2b29544c9de405b921c1ecae0054e0a3cc2393
Thor Christoffersen Hochman <thorchri@usc.edu>
2023-09-02 20:52:25 -0300

    Update FindAce.py

--- FindAce.py
+++ FindAce.py
@@ -1,6 +1,9 @@
-## write a for loop from 1 to 52 where you find the expected value of the number of cards until you see the first ace
+## Answering the Question: What is the expected number of cards until you take an ace?
+
+## write a for loop from 1 to 48 where you find the expected value of the number of cards until you see the first ace
 ## for each card, multiply the probability of seeing it by the number of cards taken to see it
 ## add that to the cumulative sum
+
 
 def probabilityOfNotGettingAce(n):
     ## n is the number of cards drawn already
@@ -21,4 +24,5 @@ for i in range(1, 49):
     cumulativeSum += (i) * probabilityOfGettingAce(i)
 
 print(cumulativeSum)
+## = 10.6
 
